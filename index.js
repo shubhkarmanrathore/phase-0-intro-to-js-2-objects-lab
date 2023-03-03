2a6d77051a9efacde796da8430286c8365f7872f
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-03-02 22:36:29 -0800

    adds js2 objects lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee = {};
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    return {...object, [key]: value,};
+    }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key,value) {
+    object[key]=value;
+    return object;
+}
+
+function deleteFromEmployeeByKey(object, key) {
+    const newObject = { ...object };
+    delete newObject[key];
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete object[key];
+    return object
+}
